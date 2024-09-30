0c7b0bc173cc8fc6adebea6b732f88d4c6faf567
sadaswda12 <141890418+sadaswda12@users.noreply.github.com>
2024-09-30 03:51:03 +0000

    Deploying to gh-pages from  @ 5d8303a2e711fea40b91ec6d093efce95152a1f0 🚀

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1727668208907";
+const VERSION = "1727668261655";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
