f1e1aab52e86da51810c14b93d8eafc4d451280a
sadaswda12 <141890418+sadaswda12@users.noreply.github.com>
2025-01-03 01:36:56 +0000

    Deploying to gh-pages from  @ 7f2a4112a4dbc6e3245e19f731fed4b01419d4d3 🚀

--- sw.js
+++ sw.js
@@ -1,13 +1,4 @@
-const preCache = [
-  "/images/taichi.png",
-  "/images/banner.webp",
-  "/images/taichi-fill.png",
-  "/css/loader.css",
-  "/css/style.css",
-  "/js/script.js",
-];
-const VERSION = "1729677993894";
-const cacheDomain = [
+const VERSION = "1735868214027";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
